53600a5de1d36eef60234b0d6830127e2424cd63
EvgeniiyaR <tulpansik@mail.ru>
2023-09-14 17:43:13 +0300

    feat: add dataset id

--- script.js
+++ script.js
@@ -4,23 +4,24 @@ const buttonAddTask = document.querySelector('.main__button_type_add');
 const formAddTask = document.querySelector('.main__form');
 const listTasks = document.querySelector('.main__list-tasks');
 const completeTasks = [];
+let count = 0;
 
 const getTemplate = () => {
   const clone = templateTask.content.cloneNode(true);
   const textTask = clone.querySelector('.main__text');
   const buttonCompleteTask = clone.querySelector('.main__button_type_complete');
   const buttonDeleteTask = clone.querySelector('.main__button_type_delete');
+  const task = clone.querySelector('.main__task');
 
-  return [clone, textTask, buttonCompleteTask, buttonDeleteTask];
+  return [clone, textTask, buttonCompleteTask, buttonDeleteTask, task];
 }
 
 const deleteTask = (e) => {
   const target = e.target.parentNode;
   const tasks = JSON.parse(localStorage.getItem('listsTasks'));
-  const text = target.querySelector('.main__text').textContent;
   listTasks.removeChild(target);
   tasks.forEach((item, index) => {
-    if (item === text) {
+    if ((tasks.length - index) === Number(target.dataset.id)) {
       tasks.splice(index, 1);
     }
   })
@@ -39,9 +40,11 @@ const completeTask = (e) => {
 
 if (localStorage.getItem('listsTasks')) {
   const tasks = JSON.parse(localStorage.getItem('listsTasks'));
-  tasks.forEach((item) => {
-    const [clone, textTask, buttonCompleteTask, buttonDeleteTask] = getTemplate();
+  count = tasks.length;
+  tasks.forEach((item, index) => {
+    const [clone, textTask, buttonCompleteTask, buttonDeleteTask, task] = getTemplate();
     textTask.textContent = item;
+    task.dataset.id = tasks.length - index;
     buttonDeleteTask.addEventListener('click', deleteTask);
     listTasks.appendChild(clone);
   })
@@ -49,11 +52,13 @@ if (localStorage.getItem('listsTasks')) {
 
 const addTask = (e) => {
   e.preventDefault();
-  const [clone, textTask, buttonCompleteTask, buttonDeleteTask] = getTemplate();
+  const [clone, textTask, buttonCompleteTask, buttonDeleteTask, task] = getTemplate();
   textTask.textContent = inputTask.value;
   inputTask.value = '';
   buttonDeleteTask.addEventListener('click', deleteTask);
   buttonCompleteTask.addEventListener('click', completeTask);
+  count += 1;
+  task.dataset.id = count;
   listTasks.prepend(clone);
   const tasks = document.querySelectorAll('.main__task');
   const arrayTasks = [];
