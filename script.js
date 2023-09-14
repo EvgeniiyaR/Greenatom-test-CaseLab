194d00f2c81e966d849bba6ddc11ddbc95996fe4
EvgeniiyaR <tulpansik@mail.ru>
2023-09-14 16:19:22 +0300

    feat: add logic addTask and save localStorage list tasks

---
+++ script.js
@@ -0,0 +1,41 @@
+const templateTask = document.querySelector('#template-task');
+const inputTask = document.querySelector('.main__input');
+const buttonAddTask = document.querySelector('.main__button_type_add');
+const formAddTask = document.querySelector('.main__form');
+const listTasks = document.querySelector('.main__list-tasks');
+
+const getTemplate = () => {
+  const clone = templateTask.content.cloneNode(true);
+  const textTask = clone.querySelector('.main__text');
+  const buttonCompleteTask = clone.querySelector('.main__button_type_complete');
+  const buttonDeleteTask = clone.querySelector('.main__button_type_delete');
+
+  return [clone, textTask, buttonCompleteTask, buttonDeleteTask];
+}
+
+if (localStorage.getItem('listsTasks')) {
+  const tasks = JSON.parse(localStorage.getItem('listsTasks'));
+  tasks.forEach((item) => {
+    const [clone, textTask, buttonCompleteTask, buttonDeleteTask] = getTemplate();
+    textTask.textContent = item;
+    listTasks.appendChild(clone);
+  })
+}
+
+const addTask = (e) => {
+  e.preventDefault();
+  const [clone, textTask, buttonCompleteTask, buttonDeleteTask] = getTemplate();
+  textTask.textContent = inputTask.value;
+  inputTask.value = '';
+
+  listTasks.appendChild(clone);
+  const tasks = document.querySelectorAll('.main__task');
+  const arrayTasks = [];
+  tasks.forEach((item) => {
+    const text = item.querySelector('.main__text').textContent;
+    arrayTasks.push(text);
+  });
+  localStorage.setItem('listsTasks', JSON.stringify(arrayTasks));
+}
+
+formAddTask.addEventListener('submit', addTask);
