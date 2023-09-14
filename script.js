b75f8f4dd303f2e0046d2cc14249fe4fd971e1c2
EvgeniiyaR <tulpansik@mail.ru>
2023-09-14 20:59:34 +0300

    feat: add logic completeTask/uncompleteTask

--- script.js
+++ script.js
@@ -3,8 +3,18 @@ const inputTask = document.querySelector('.main__input');
 const buttonAddTask = document.querySelector('.main__button_type_add');
 const formAddTask = document.querySelector('.main__form');
 const listTasks = document.querySelector('.main__list-tasks');
-const completeTasks = [];
-let count = 0;
+
+if (inputTask.value === '') {
+  buttonAddTask.disabled = true;
+}
+
+inputTask.addEventListener('input', (e) => {
+  if (e.target.value === '') {
+    buttonAddTask.disabled = true;
+  } else {
+    buttonAddTask.disabled = false;
+  }
+})
 
 const getTemplate = () => {
   const clone = templateTask.content.cloneNode(true);
@@ -18,55 +28,104 @@ const getTemplate = () => {
 
 const deleteTask = (e) => {
   const target = e.target.parentNode;
-  const tasks = JSON.parse(localStorage.getItem('listsTasks'));
+  const arrayTasks = JSON.parse(localStorage.getItem('listTasks'));
+  const completeTasks = JSON.parse(localStorage.getItem('completeTasks'));
+
   listTasks.removeChild(target);
-  tasks.forEach((item, index) => {
-    if ((tasks.length - index) === Number(target.dataset.id)) {
-      tasks.splice(index, 1);
+  arrayTasks.forEach((item, index) => {
+    if (item[1] === Number(target.dataset.id)) {
+      arrayTasks.splice(index, 1);
     }
   })
-  localStorage.setItem('listsTasks', JSON.stringify(tasks));
+  if (completeTasks) {
+    completeTasks.forEach((completeTask, completeIndex) => {
+      if (completeTask === Number(target.dataset.id)) {
+        completeTasks.splice(completeIndex, 1);
+      }
+    })
+  }
+  localStorage.setItem('listTasks', JSON.stringify(arrayTasks));
+  localStorage.setItem('completeTasks', JSON.stringify(completeTasks));
 }
 
 const completeTask = (e) => {
   const target = e.target.parentNode;
+  e.target.textContent = 'Не готово';
   const text = target.querySelector('.main__text');
-  completeTasks.push(text.textContent);
-  localStorage.setItem('completeTasks', JSON.stringify(completeTasks));
+  const arrayTasks = JSON.parse(localStorage.getItem('listTasks'));
+  const completeTasks = JSON.parse(localStorage.getItem('completeTasks')) || [];
+  arrayTasks.forEach((item, index) => {
+    if (item[1] === Number(target.dataset.id)) {
+      arrayTasks.splice(index, 1);
+    }
+  })
+  arrayTasks.push([text.textContent, Number(target.dataset.id)]);
   text.classList.add('main__text_type_complete');
   listTasks.appendChild(target);
+  completeTasks.push(Number(target.dataset.id));
+  localStorage.setItem('listTasks', JSON.stringify(arrayTasks));
+  localStorage.setItem('completeTasks', JSON.stringify(completeTasks));
+}
+
+const uncompleteTask = (e) => {
+  const target = e.target.parentNode;
+  e.target.textContent = 'Готово';
+  const text = target.querySelector('.main__text');
+  const completeTasks = JSON.parse(localStorage.getItem('completeTasks')) || [];
+  completeTasks.forEach((item, index) => {
+    if (item === Number(target.dataset.id)) {
+      completeTasks.splice(index, 1);
+    }
+  })
+  text.classList.remove('main__text_type_complete');
+  localStorage.setItem('completeTasks', JSON.stringify(completeTasks));
 }
 
 
-if (localStorage.getItem('listsTasks')) {
-  const tasks = JSON.parse(localStorage.getItem('listsTasks'));
-  count = tasks.length;
-  tasks.forEach((item, index) => {
+if (localStorage.getItem('listTasks')) {
+  const completeTasks = JSON.parse(localStorage.getItem('completeTasks'));
+  const arrayTasks = JSON.parse(localStorage.getItem('listTasks'));
+  arrayTasks.forEach((item) => {
     const [clone, textTask, buttonCompleteTask, buttonDeleteTask, task] = getTemplate();
-    textTask.textContent = item;
-    task.dataset.id = tasks.length - index;
-    buttonDeleteTask.addEventListener('click', deleteTask);
+    textTask.textContent = item[0];
+    task.dataset.id = item[1];
     listTasks.appendChild(clone);
+    completeTasks.forEach((completeTask) => {
+      if (completeTask === item[1]) {
+        textTask.classList.add('main__text_type_complete');
+      }
+    })
+    buttonDeleteTask.addEventListener('click', deleteTask);
+    buttonCompleteTask.addEventListener('click', (e) => {
+      if (textTask.classList.contains('main__text_type_complete')) {
+        uncompleteTask(e);
+      } else {
+        completeTask(e);
+      }
+    });
   })
 }
 
 const addTask = (e) => {
   e.preventDefault();
+  const arrayTasks = JSON.parse(localStorage.getItem('listTasks')) || [];
+  let count = arrayTasks.length;
   const [clone, textTask, buttonCompleteTask, buttonDeleteTask, task] = getTemplate();
   textTask.textContent = inputTask.value;
   inputTask.value = '';
-  buttonDeleteTask.addEventListener('click', deleteTask);
-  buttonCompleteTask.addEventListener('click', completeTask);
   count += 1;
   task.dataset.id = count;
+  arrayTasks.unshift([textTask.textContent, count]);
   listTasks.prepend(clone);
-  const tasks = document.querySelectorAll('.main__task');
-  const arrayTasks = [];
-  tasks.forEach((item) => {
-    const text = item.querySelector('.main__text').textContent;
-    arrayTasks.push(text);
+  localStorage.setItem('listTasks', JSON.stringify(arrayTasks));
+  buttonDeleteTask.addEventListener('click', deleteTask);
+  buttonCompleteTask.addEventListener('click', (e) => {
+    if (textTask.classList.contains('main__text_type_complete')) {
+      uncompleteTask(e);
+    } else {
+      completeTask(e);
+    }
   });
-  localStorage.setItem('listsTasks', JSON.stringify(arrayTasks));
 }
 
 formAddTask.addEventListener('submit', addTask);
